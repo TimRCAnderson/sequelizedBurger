42f624dfab03c7a97492be29b25e62e607549df6
TimRCAnderson <timothyrcanderson@gmail.com>
2017-06-10 06:30:12 -0400

    changed soution to use Sequelize ORM.

---
+++ controllers/burgersController.js
@@ -0,0 +1,40 @@
+var express = require("express");
+
+var router = express.Router();
+var db = require("../models");
+
+// get route -> index
+router.get("/", function(req, res) {
+  res.redirect("/burgers");
+});
+
+router.get("/burgers", function(req, res) {
+  // express callback response by calling burger.selectAllBurger
+  db.Burger.findAll({}).then(function(burgerData) {
+    // wrapper for orm.js that using MySQL query callback will return burger_data, render to index with handlebar
+    res.render("index", { burger_data: burgerData });
+  });
+});
+
+// post route -> back to index
+router.post("/burgers/create", function(req, res) {
+  // takes the request object using it as input for buger.addBurger
+  db.Burger.create({burgerName: req.body.burgerName}).then(function(result) {
+    // wrapper for orm.js that using MySQL insert callback will return a log to console,
+    // render back to index with handle
+    console.log(result);
+    res.redirect("/");
+  });
+});
+
+// put route -> back to index
+router.put("/burgers/update", function(req, res) {
+  db.Burger.update({devoured: true}, {where: {id: req.body.burger_id}}).then(function(result) {
+    // wrapper for orm.js that using MySQL update callback will return a log to console,
+    // render back to index with handle
+    console.log(result);
+    res.redirect("/");
+  });
+});
+
+module.exports = router;
---
+++ models/burger.js
@@ -0,0 +1,24 @@
+
+
+
+
+
+
+
+
+module.exports = function(sequelize, DataTypes) {
+  var Burger = sequelize.define("Burger", {
+    burgerName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [1, 140]
+      }
+    },
+    devoured: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: false
+    }
+  });
+  return Burger;
+};
